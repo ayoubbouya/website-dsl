efad37a720440ef3d0e23a3440b040a7bde6b883
ayoubbouya <bouya.ayoub1@gmail.com>
2021-02-19 00:21:54 +0200

    updated

--- src/Clients/Companies/Companies.js
+++ src/Clients/Companies/Companies.js
@@ -2,7 +2,7 @@ import React from "react";
 import Company from "./Company";
 import company1 from "./image17.svg";
 import company2 from "./image16.svg";
-
+import company3 from "./company3.svg";
 import line from "../../About-team/management/images/lineRed.svg";
 import styles from "./styles.module.css";
 function Companies() {
@@ -33,7 +33,28 @@ function Companies() {
         headingH2="Mobile Banking :"
         paragraph1="This project provides delivery channel application over mobile device for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment.
         Using: JAVA technologies, PostgreSQL, Android technologies, iOS technologies, Blackberry technologies "
+        headingH2T2="Soft Token :"
+        paragraph2T2="This project provides software application to generate token secure code from mobile device used to authenticate banking and financial transaction from delivery channel performed by customer.
+        Using: JAVA technologies, PostgreSQL, Android technologies, iOS technologies, Blackberry technologies"
+        headingH2T3="Internet Banking :"
+        paragraph2T3="This project provides delivery channel application over web browser for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment.
+        Using: JAVA technologies, PostgreSQL"
+        headingH2T4="KIOSK :"
+        paragraph2T4="This project provides non-cash delivery channel machine and software application for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment.
+        Using: JAVA technologies, .NET framework, PostgreSQL"
       />
+      <div className={styles.bg}>
+        <Company
+          image={company3}
+          headingH1="Bank BPR KS"
+          headingH2="Mobile Banking :"
+          paragraph1="This project provides delivery channel application over mobile device for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment.
+          Using: JAVA technologies, PostgreSQL, Android technologies, iOS technologies, Blackberry technologies "
+          headingH2T2="Internet Banking :"
+          paragraph2T2="This project provides delivery channel application over web browser for customer to perform banking and financial transactions, such as inquiry balance, overbook, purchase, and payment.
+          Using: JAVA technologies, PostgreSQL"
+        />
+      </div>
     </div>
   );
 }
